0e8be85d8a766c294b8cd5c9bcb98eb90f87de47
apros7 <lucas.vilsen@gmail.com>
2025-08-11 13:44:42 +0200

    Initial Commit

---
+++ Outflow/OutflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutflowApp.swift
+//  Outflow
+//
+//  Created by Lucas Vilsen on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct OutflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OutflowTests/OutflowTests.swift
@@ -0,0 +1,16 @@
+//
+//  OutflowTests.swift
+//  OutflowTests
+//
+//  Created by Lucas Vilsen on 11/08/2025.
+//
+
+import Testing
+
+struct OutflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
